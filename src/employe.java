0d643d29e2ec2f9a9d65497fffc8381609b4b4fe
MIMOC2016 <meriyamabane@gmail.Com>
2016-06-29 10:14:33 +0200

    1ere classe

---
+++ src/employe.java
@@ -0,0 +1,15 @@
+
+public class employe 
+{
+ String nom;
+ String prenom;
+ int age;
+ 
+ public employe (String nom, String prenom, int age)
+ {
+	 this.nom = nom;
+	 this.prenom = prenom;
+	 this.age = age;
+ }
+
+}
