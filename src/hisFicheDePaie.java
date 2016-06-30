03138a680810a75b19d3926cfd59736d773223d3
azzaoui5 <azzaoui.m@gmail.com>
2016-06-30 15:44:16 +0200

    Create hisFicheDePaie.java

---
+++ src/hisFicheDePaie.java
@@ -0,0 +1,14 @@
+public class hisFicheDePaie
+{
+ String dateCreation;
+ String dateDeb;
+ String dateFin;
+ 
+ public hisFicheDePaie (String dateCreation, String dateDeb, String dateFin)
+ {
+ 	 this.dateCreation = dateCreation;
+ 	 this.dateDeb = dateDeb;
+ 	 this.dateFin = dateFin;
+ }
+
+}
