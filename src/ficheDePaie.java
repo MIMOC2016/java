41c11757f930d40629d176a89e4e9d8a9131bc5f
azzaoui5 <azzaoui.m@gmail.com>
2016-06-30 15:47:01 +0200

    Create ficheDePaie.java

---
+++ src/ficheDePaie.java
@@ -0,0 +1,12 @@
+public class ficheDePaie
+{
+ float salaireVariable;
+ float salaireFixe;
+ 
+ public ficheDePaie (float salaireVariable, float salaireFixe)
+ {
+ 	 this.salaireVariable = salaireVariable;
+ 	 this.salaireFixe = salaireFixe;
+ }
+
+}
