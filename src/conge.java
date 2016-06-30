3345aee38ab7b8321cd3a31ca18aa590c137c6b1
azzaoui5 <azzaoui.m@gmail.com>
2016-06-30 15:31:44 +0200

    Create conge.java

---
+++ src/conge.java
@@ -0,0 +1,15 @@
+
+public class conge
+{
+ String dateDeb;
+ String dateFin;
+ String motif;
+ 
+ public conge (String dateDeb, String dateFin, String motif)
+ {
+ 	 this.dateDeb = dateDeb;
+ 	 this.dateFin = dateFin;
+ 	 this.motif = motif;
+ }
+
+}
