e095eb9aeb2d22f56d4d26a9e2f9a3fdf616fa3f
azzaoui5 <azzaoui.m@gmail.com>
2016-06-30 15:29:34 +0200

    Create historiqueConge.java

---
+++ src/historiqueConge.java
@@ -0,0 +1,15 @@
+
+public class historiqueConge
+{
+ String dateDemande;
+ String dateAcceptation;
+
+ 
+ public employe (String dateDemande, String dateAcceptation)
+ {
+ 	 this.dateDemande = dateDemande;
+ 	 this.dateAcceptation = dateAcceptation;
+ 	 
+ }
+
+}
