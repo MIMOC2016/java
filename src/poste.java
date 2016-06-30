0d6623b529611f290e9b2de32448bd60464ac891
azzaoui5 <azzaoui.m@gmail.com>
2016-06-30 15:49:13 +0200

    Create poste.java

---
+++ src/poste.java
@@ -0,0 +1,13 @@
+public class poste
+{
+ String nomPoste;
+ String departement;
+
+ 
+ public poste (String nomPoste, String departement)
+ {
+ 	 this.nomPoste = nomPoste;
+ 	 this.departement = departement;
+ }
+
+}
