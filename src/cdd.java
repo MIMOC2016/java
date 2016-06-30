05c83590bc7183e20881058aaf4549ba63fb56b0
azzaoui5 <azzaoui.m@gmail.com>
2016-06-30 15:41:49 +0200

    Create cdd.java

---
+++ src/cdd.java
@@ -0,0 +1,13 @@
+public class cdd 
+{
+ String datFinContrat;
+ String typeContrat;
+
+ 
+ public cdd (String dateFinContrat, String typeContrat)
+ {
+ 	 this.dateFinContrat = dateFinContrat;
+ 	 this.typeContrat = typeContrat;
+ }
+
+}
