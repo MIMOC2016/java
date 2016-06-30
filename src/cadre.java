51afeccd54bf974f162e2943f0e54b5e3fdc9b91
azzaoui5 <azzaoui.m@gmail.com>
2016-06-30 15:34:08 +0200

    Create cadre.java

---
+++ src/cadre.java
@@ -0,0 +1,13 @@
+
+public class cadre
+{
+ int tauxJour;
+ int jourPresence
+ 
+ public cadre (int tauxJour, int jourPresence)
+ {
+ 	 this.tauxJour = tanxJour;
+ 	 this.jourPresence = jourPresence;
+ }
+
+}
