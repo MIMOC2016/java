543dad830d849838201867543eb9c4646ac5b063
azzaoui5 <azzaoui.m@gmail.com>
2016-06-30 15:23:56 +0200

    Update employe.java

--- src/employe.java
+++ src/employe.java
@@ -1,15 +1,23 @@
 
 public class employe 
 {
+ int idEmp;
  String nom;
  String prenom;
- int age;
+ String dateEmbauche;
+ String dateNaissance;
+ String tel;
+ String email;
  
- public employe (String nom, String prenom, int age)
+ public employe (int idEmp, String nom, String prenom, String dateEmbauche, String dateNaissance, String tel, String email)
  {
+ 	 this.idEmp = idEmp;
 	 this.nom = nom;
 	 this.prenom = prenom;
-	 this.age = age;
+	 this.dateEmbauche = dateEmbauche;
+	 this.dateNaissance = dateNaissance;
+	 this.tel = tel;
+	 this.email = email;
  }
 
 }
