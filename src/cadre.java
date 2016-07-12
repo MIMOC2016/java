ed66cafc2623ff9fecf7cf8a77a14a064e7e8490
azzaoui5 <azzaoui.m@gmail.com>
2016-07-12 15:13:25 +0200

    Update cadre.java

--- src/cadre.java
+++ src/cadre.java
@@ -10,5 +10,19 @@ public class cadre extends employe
  	 this.tauxJour = tanxJour;
  	 this.jourPresence = jourPresence;
  }
+ 
+ public int getTauxJour(){
+  return tauxJour;
+ }
+ 
+  public int getJourPresence(){
+  return jourPresence;
+ }
+ public void setTauxJour(int tj){
+   this.tauxJour = tj;
+}
 
+ public void setJourPresence(int jp){
+   this.jourPresence = jp;
+}
 }
