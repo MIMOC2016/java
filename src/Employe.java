f87f6bb810e9760f60c654d87902c4926b554605
MIMOC2016 <meriyamabane@gmail.com>
2016-07-12 17:28:57 +0200

    employe

---
+++ src/Employe.java
@@ -0,0 +1,25 @@
+
+public class Employe 
+{
+ int idEmp;
+ private static int cpt=0;
+ private String nom;
+ private String prenom;
+ private String dateEmbauche;
+ private String dateNaissance;
+ private String tel;
+ private String email;
+ 
+ public Employe (String nom, String prenom, String dateEmbauche, String dateNaissance, String tel, String email)
+ {
+	 this.nom = nom;
+	 this.prenom = prenom;
+	 this.dateEmbauche = dateEmbauche;
+	 this.dateNaissance = dateNaissance;
+	 this.tel = tel;
+	 this.email = email;
+ 	 this.idEmp = cpt++;
+
+ }
+
+}
