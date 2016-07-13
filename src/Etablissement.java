4a8ee0a2cad60a4466fc8f172eff23936c1eefb2
VivianeCHEN <chen.viviane@hotmail.fr>
2016-07-13 10:19:33 +0200

    Update and rename etablissement.java to Etablissement.java
    Rajout majuscule à la classe

---
+++ src/Etablissement.java
@@ -0,0 +1,14 @@
+
+public class Etablissement 
+{
+ private int idEtab;
+ private String adr;
+
+ 
+ public Etablissement (int idEmp, String adr)
+ {
+ 	 this.idEmp = idEmp;
+	 this.adr = adr;
+ }
+
+}
