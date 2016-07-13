b71f9c5df1801a1fa30118b3fc61b7360257719f
MIMOC2016 <meriyamabane@gmail.Com>
2016-07-13 18:09:22 +0200

    modif objet paie

--- src/TestEmploye.java
+++ src/TestEmploye.java
@@ -1,13 +1,19 @@
+ 
+
 
 public class TestEmploye {
 
 	public static void main(String[] args) 
 	
+	
 	{
-Employe a = new Employe ("TOTO", "Tutu", "11/06/2016", "12/07/1990", "060606060606", "toto@gmail.com");
+//--------------saisir une paie---------------------------//
 
-System.out.println(a);
+		CalculPaie paiefev = new CalculPaie();
+		String paie = paiefev.ToString();
+		System.out.println(paie);
 
-	}
+}
 
+	
 }
