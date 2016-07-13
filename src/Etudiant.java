fc54250f34abd14beb8735e03761b8554c19cc37
VivianeCHEN <chen.viviane@hotmail.fr>
2016-07-13 21:42:25 +0200

    Update Etudiant.java
    Constructeur modifié

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -5,8 +5,9 @@ public class Etudiant extends Employe
  private String cursus;
 
  
- public Etudiant (String ecole, String dateFinContrat, String cursus)
+ public Etudiant (String nom, String prenom, String dateEmbauche, String dateNaissance, String tel, String email, String ecole, String dateFinContrat, String cursus)
  {
+	 super(nom,prenom,dateEmbauche,dateNaissance,tel,email);
  	 this.ecole = ecole;
  	 this.dateFinContrat = dateFinContrat;
  	 this.cursus = cursus;
