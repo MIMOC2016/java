1cef8de2e277610639cd94e805748d3549521aa7
VivianeCHEN <chen.viviane@hotmail.fr>
2016-07-13 22:41:38 +0200

    Update Alternant.java

--- src/Alternant.java
+++ src/Alternant.java
@@ -4,9 +4,9 @@ public class Alternant extends Etudiant implements Conge
  private boolean enCong;
 
  
- public Alternant (float salaire)
+ public Alternant (String nom, String prenom, String dateEmbauche, String dateNaissance, String tel, String email, String ecole, String dateFinContrat, String cursus, float salaire)
  {
-   super();
+   super(nom,prenom,dateEmbauche,dateNaissance,tel,email,ecole,dateFinContrat,cursus);
  	 this.salaire = salaire;
  }
 public float getSalaire(){
