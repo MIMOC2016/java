7570f64ba57c6361da8b6b24840dc51f57f8c0f7
azzaoui5 <azzaoui.m@gmail.com>
2016-07-13 15:57:58 +0200

    Update Etudiant.java

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,15 +1,13 @@
 public class Etudiant extends Employe
 {
  private String ecole;
- private String dateDebContrat;
  private String dateFinContrat;
  private String cursus;
 
  
- public Etudiant (String ecole, String dateDebContrat, String dateFinContrat, String cursus)
+ public Etudiant (String ecole, String dateFinContrat, String cursus)
  {
  	 this.ecole = ecole;
- 	 this.dateDebContrat = dateDebContrat;
  	 this.dateFinContrat = dateFinContrat;
  	 this.cursus = cursus;
  }
@@ -17,9 +15,7 @@ public class Etudiant extends Employe
 public String getEcole(){
    return ecole;
 }
-public String getDateDebContrat(){
-   return dateDebContrat;
-}
+
 public String getDateFinContrat(){
    return dateFinContrat;
 }
@@ -31,9 +27,6 @@ public void setEcole(String e){
  this.ecole = e;
 }
 
-public void setDateDebContrat(String dd){
- this.dateDebContrat = dd;
-}
 
 public void setDateFinContrat(String df){
  this.dateFinContrat = df;
