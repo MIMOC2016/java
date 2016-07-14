f1b487c7987c04cb95b5adf432aeb74f9709fd89
VivianeCHEN <chen.viviane@hotmail.fr>
2016-07-14 21:22:31 +0200

    Interface -Employés qui peuvent prendre des congés

---
+++ src/EmployeConge.java
@@ -0,0 +1,24 @@
+public class EmployeConge extends Employe implements Conge {
+	private boolean enCong=false;
+	
+	public EmployeConge(String nom, String prenom, String dateEmbauche, String dateNaissance, String tel, String email){
+		super(nom,prenom,dateEmbauche,dateNaissance,tel,email);
+	}
+	
+	 public void debutConge() {
+		   this.enCong=true;
+		}
+
+	public void finConge() {
+		   this.enCong=false;
+		}
+
+	public boolean enConge() {
+		   return this.enCong;
+		}
+		
+	public String toString(){
+		  return super.toString()+ ( enConge() ? ", en congé" : "");
+					
+		}
+}
