2f8036f768faf30f3ba93065060345175e9f8f7d
VivianeCHEN <chen.viviane@hotmail.fr>
2016-07-14 22:08:49 +0200

    OK

--- src/Stagiaire.java
+++ src/Stagiaire.java
@@ -1,18 +1,40 @@
 public class Stagiaire extends Etudiant
 {
- private float gratification;
+	private float gratification;
+	private String perpaie;
 
+	public Stagiaire(String nom, String prenom, String dateEmbauche, String dateNaissance, int tel, String email, String ecole, String dateFinContrat, String cursus, float gratification)
+ 		{
+	 		super(nom,prenom,dateEmbauche,dateNaissance,tel,email,ecole, dateFinContrat, cursus);
+	 		this.gratification = gratification;
+ 		}
  
- public Stagiaire (float gratification)
- {
-   super();
- 	 this.gratification = gratification;
- }
-public float getGratification(){
-   return gratification;
-}
+	public float getGratification(){
+		return gratification;
+	}
 
-public void setGratification(float g){
- this.gratification = g;
-}
+	public void setGratification(float g){
+		this.gratification = g;
+	}
+	
+	public String toString(){
+		return "n°" + idEmp+"\n"+
+				nom + prenom + "né le " + dateNaissance+ "\n"+
+				"tel: "+ tel + "\n"+
+				"email: "+email+ "\n"+
+				"date d'Embauche" +dateEmbauche+ "\n"+
+				"date Fin Contrat : "+ dateFinContrat +"\n"+
+				"école : "+ ecole+"\n"+
+				"cursus : "+cursus+"\n"+
+				"gratification : "+gratification+"\n";
+		
+	}
+	
+	//----------------------------METHODE CALCUL PAIE CADRE---------------------------//
+
+		public Paie calculPaie()
+		{	
+			return new Paie(this.perpaie,this.gratification);
+		}
+	
 }
