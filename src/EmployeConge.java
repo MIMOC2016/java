7cba294885012e29f4d6cab215d0731ed2f09945
VivianeCHEN <chen.viviane@hotmail.fr>
2016-07-14 22:33:29 +0200

    MANQUE calcPaie

--- src/EmployeConge.java
+++ src/EmployeConge.java
@@ -1,8 +1,8 @@
 public class EmployeConge extends Employe implements Conge {
 	private boolean enCong=false;
 	
-	public EmployeConge(String nom, String prenom, String dateEmbauche, String dateNaissance, int tel, String email){
-		super(nom,prenom,dateEmbauche,dateNaissance,tel,email);
+	public EmployeConge(String nom, String prenom, String dateEmbauche, int etabAffectation, String dateNaissance, int tel, String email){
+		super(nom,prenom,dateEmbauche,etabAffectation, dateNaissance,tel,email);
 	}
 	
 	 public void debutConge() {
