3431ceaf48c552e857489445306641db3060ed12
VivianeCHEN <chen.viviane@hotmail.fr>
2016-07-14 21:59:54 +0200

    OK

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,11 +1,11 @@
-public class Etudiant extends Employe
+public abstract class Etudiant extends Employe
 {
  private String ecole;
  private String dateFinContrat;
  private String cursus;
 
  
- public Etudiant (String nom, String prenom, String dateEmbauche, String dateNaissance, String tel, String email, String ecole, String dateFinContrat, String cursus)
+ public Etudiant (String nom, String prenom, String dateEmbauche, String dateNaissance, int tel, String email, String ecole, String dateFinContrat, String cursus)
  {
 	 super(nom,prenom,dateEmbauche,dateNaissance,tel,email);
  	 this.ecole = ecole;
@@ -24,17 +24,29 @@ public String getCursus(){
    return cursus;
 }
 
-public void setEcole(String e){
- this.ecole = e;
+public void setEcole(String ecole){
+ this.ecole = ecole;
 }
 
-
-public void setDateFinContrat(String df){
- this.dateFinContrat = df;
+public void setDateFinContrat(String dateFinContrat){
+ this.dateFinContrat = dateFinContrat;
 }
 
-public void setCursus(String c){
- this.cursus = c;
+public void setCursus(String cursus){
+ this.cursus = cursus;
 }
 
+public String toString(){
+	return "n°" + idEmp+"\n"+
+			nom + prenom + "né le " + dateNaissance+ "\n"+
+			"tel: "+ tel + "\n"+
+			"email: "+email+ "\n"+
+			"date d'Embauche" +dateEmbauche+ "\n"+
+			"date Fin Contrat : "+ dateFinContrat +"\n"+
+			"école : "+ ecole+"\n"+
+			"cursus : "+cursus+"\n";
 }
+
+public abstract Paie calculPaie();
+}
+
