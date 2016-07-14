7e09a06c77d70032d31944c016ca926d9e794ec5
VivianeCHEN <chen.viviane@hotmail.fr>
2016-07-14 21:30:39 +0200

    Ajout des set et get pour tous les attributs+toString

--- src/Employe.java
+++ src/Employe.java
@@ -7,11 +7,11 @@ public abstract class Employe
  private String prenom;
  private String dateEmbauche;
  private String dateNaissance;
- private String tel;
+ private int tel;
  private String email;
  
- public Employe (String nom, String prenom, String dateEmbauche, String dateNaissance, String tel, String email)
- {
+ public Employe (String nom, String prenom, String dateEmbauche, String dateNaissance, int tel, String email)
+  {
 	 this.nom = nom;
 	 this.prenom = prenom;
 	 this.dateEmbauche = dateEmbauche;
@@ -20,9 +20,63 @@ public abstract class Employe
 	 this.email = email;
  	 this.idEmp = cpt++;
 
- }
- 
+  }
+
+	public String getNom(){
+		return nom;
+	}
+
+	public void setNom(String nom){
+		this.nom = nom;
+	}
+	
+	public String getPrenom(){
+		return prenom;
+	}
+
+	public void setPrenom(String prenom){
+		this.prenom = prenom;
+	}
+	
+	public String getDateEmbauche(){
+		return dateEmbauche;
+	}
+
+	public void setDateEmbauche(String dateEmbauche){
+		this.dateEmbauche = dateEmbauche;
+	}
+	
+	public String getDateNaissance(){
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(String dateNaissance){
+		this.dateNaissance = dateNaissance;
+	}
+	
+	public int getTel(){
+		return tel;
+	}
+
+	public void setTel(int tel){
+		this.tel = tel;
+	}
+	
+	public String getEmail(){
+		return email;
+	}
+
+	public void setEmail(String email){
+		this.email = email;
+	}
+		
+ public String toString(){
+		return "n°" + idEmp+"\n"+
+				nom + prenom + "né le" + dateNaissance+ "\n"+
+				"tel:"+ tel + "\n"+
+				"email:"+email+ "\n"+
+				"date d'Embauche" +dateEmbauche+ "\n";
+	}
+
  public abstract Paie calculPaie();
-
-
 }
