5b1436fac6fae7ab15d5d1805f777ab9711591ff
azzaoui5 <azzaoui.m@gmail.com>
2016-07-20 11:51:56 +0200

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,11 @@
 public class Test{
   public static void main(String[] args) {
+    
+    
+    Cadre c1= new Cadre("Jean", "Bernard", "01/04/2010", 1, "07/01/1974", 06651420032, "jb@gmail.com", 47000.0, 4000.0 );
+    Cadre c2= new Cadre("Mathieu", "Rimel", "01/07/2012", 2, "14/09/1984", 0604210547, "mr@gmail.com", 41000.0, 3500.0 );
+    Cdd cdd1= new Cdd("Racha", "Iskan", "15/08/2016", 1, "05/12/1990", 0741254615, "ri@gmail.com", "01/07/2017" 28000.0, 1400.0 );
+    Cdd cdd2= new Cdd("Sarah", "Tarram", "01/07/2016", 1, "07/08/1994", 0741254132, "st@gmail.com", "14/09/2017" 35000.0, 1700.0 );
+    
   }
 }
